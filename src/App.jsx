a1fd88e7ef0ce6cd6c62985d8607bc2bea29a7ab
SuggestedName <lpham1357@gmail.com>
2021-11-19 10:35:46 -0500

    update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import About from "./component/jsx/About";
+import Intro from "./component/jsx/Intro";
+import Works from "./component/jsx/Works";
+
+const App = () => {
+  return (
+    <div>
+      <Intro />
+      <About />
+      <Works />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/jsx/Works.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../css/works.css";
+const Works = () => {
+  return (
+    <div className="w">
+      <div className="w-wrap">
+        <div className="w-grid">
+          <div className="w-card">
+            <div className="w-content"></div>
+          </div>
+          <div className="w-card">
+            <div className="w-content"></div>
+          </div>
+          <div className="w-card">
+            <div className="w-content"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
