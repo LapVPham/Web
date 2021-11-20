65b0da48c7bf55ab7487e995bb98a1405ec337ce
SuggestedName <lpham1357@gmail.com>
2021-11-19 21:53:16 -0500

    works section

--- src/component/jsx/Works.jsx
+++ src/component/jsx/Works.jsx
@@ -4,15 +4,43 @@ const Works = () => {
   return (
     <div className="w">
       <div className="w-wrap">
+        <h1 className="w-title">Works</h1>
         <div className="w-grid">
           <div className="w-card">
-            <div className="w-content"></div>
+            <div className="w-content">
+              <h2 className="w-subtitle">Project 1</h2>
+              <p className="w-desc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat duis aute irure dolor in
+                reprehende.
+              </p>
+            </div>
           </div>
           <div className="w-card">
-            <div className="w-content"></div>
+            <div className="w-content">
+              <h2 className="w-subtitle">Project 2</h2>
+              <p className="w-desc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat duis aute irure dolor in
+                reprehende.
+              </p>
+            </div>
           </div>
           <div className="w-card">
-            <div className="w-content"></div>
+            <div className="w-content">
+              <h2 className="w-subtitle">Project 3</h2>
+              <p className="w-desc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat duis aute irure dolor in
+                reprehende.
+              </p>
+            </div>
           </div>
         </div>
       </div>
